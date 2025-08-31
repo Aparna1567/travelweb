39bef505c28f14e01c3ae9400a77958afbb6264a
Aparna1567 <aparnaapar99@gmail.com>
2025-08-31 07:33:31 +0530

    syncfusion updated successfully

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY)
 import {
   isRouteErrorResponse,
   Links,
@@ -22,8 +24,7 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
-import {registerLicense} from '@syncfusion/ej2-base';
-registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY)
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -67,6 +68,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
     <main className="pt-16 p-4 container mx-auto">
       <h1>{message}</h1>
       <p>{details}</p>
+      
       {stack && (
         <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
--- app/routes/root/page-layout.tsx
+++ app/routes/root/page-layout.tsx
@@ -1,19 +1,41 @@
 import { useNavigate } from "react-router";
+import {useState} from 'react';
 import { logoutUser } from "~/appwrite/auth";
 
 const PageLayout = () => {
+    const [loading,setLoading]=useState(false);
     const navigate=useNavigate();
+
+    const handleDashboard=()=>{
+        setLoading(true);
+        setTimeout(()=>{
+            navigate('/dashboard')
+        },1000);
+    };
  const handleLogout =async () =>{
     await logoutUser();
     navigate('/sign-in')
    }
+
+//    onClick={()=>{navigate('/dashboard')}}
   return (
     <div className='flex items-center justify-center min-h-screen bg-gray-100'>
         <div className='bg-gray-300 shadow-lg rounded-2xl p-8 w-80 text-center'>
         <h1 className='text-2xl font-bold mb-6 text-gray-800'>Welcome</h1>
         <div className='flex flex-col gap-4'>
-        <button onClick={()=>{navigate('/dashboard')}} className='cursor-pointer px-4 py-2 bg-blue-600 text-white rounded-xl
-        shadow hover:bg-blue-700 transition duration-200'>Dashboard</button>
+        <button disabled={loading} onClick={handleDashboard} className='cursor-pointer px-4 py-2 bg-blue-600 text-white rounded-xl
+        shadow hover:bg-blue-700 transition duration-200'>
+            {loading ? (
+                <div className='flex items-center gap-2'>
+                    <svg className="animate-spin h-5 w-5 text-white" xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+                    <path className="opacity-75" fill="currentColor" d='M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z'></path>
+                    </svg>Loading...          
+                </div>
+            ) : (
+                    "Dashboard"
+            )}
+            </button>
 
         <button
                         onClick={handleLogout}
